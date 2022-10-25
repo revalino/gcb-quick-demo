537c1a5de438d8c5ce94ed45bae67a4c844ae706
Revalino Sandoval <revalino@elisaks.altostrat.com>
2022-10-25 12:27:07 +0000

    Simplificando cloudbuild

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return "Hello World (Pythoni)!!! (up %s)\n" % elapsed()
+    return "Dummy App (Python)!!! (up since %s)\n" % elapsed()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
